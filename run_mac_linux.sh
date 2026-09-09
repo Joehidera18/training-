651ce65e6226ff5938e8cbc4a5e58af5ffe6ae4e
Joehidera18 <gabrielbriseno066@gmail.com>
2026-09-09 00:47:09 -0600

    Delete run_mac_linux.sh

--- run_mac_linux.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-cd "$(dirname "$0")"
-if [ ! -d .venv ]; then python3 -m venv .venv; fi
-source .venv/bin/activate
-python -m pip install -r requirements.txt
-export OPEN_BROWSER=1
-python app.py
